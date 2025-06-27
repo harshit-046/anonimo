392c176a2bf58d4754eb6d37f705027ef02dd59a
harshit-046 <147156800+harshit-046@users.noreply.github.com>
2025-06-27 15:57:51 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,7 +19,6 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next();
 }
  
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
     '/signin',
